6f2c647dbb8e57c1d17736c95b7bad04883c832f
NVP-05 <mkPhuc123456789@gmail.com>
2024-08-13 08:35:22 +0700

    add

---
+++ session01/app/Bai3/Bai3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bai3() {
+  return (
+    <div>
+      <b>Họ và tên: Nguyễn Văn Phúc</b>
+    </div>
+  )
+}
---
+++ session01/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Bai3 from './Bai3/Bai3'
+
+export default function page() {
+  return (
+    <div>
+      <Bai3></Bai3>
+    </div>
+  )
+}
